fb7ba4cce0f92f1e3f57d798221c384bcde8ccdc
ayush007-verma <ayush3528.be20@chitkara.edu.in>
2023-10-17 15:25:42 +0530

    Completed Setup Project Structure

---
+++ src/main/java/io/mountblue/blogapplication/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package io.mountblue.blogapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeHandler() {
+        return "Home";
+    }
+}
