cb034ebc9ab0c6d4b56161c4c08649219168cefa
ayush007-verma <ayush3528.be20@chitkara.edu.in>
2023-10-18 12:04:27 +0530

    Created /login and /signup added forms and handled some css

---
+++ src/main/java/io/mountblue/blogapplication/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package io.mountblue.blogapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginHandler() {
+        return "Login/Login";
+    }
+}
+
+
+
---
+++ src/main/java/io/mountblue/blogapplication/controllers/SignupController.java
@@ -0,0 +1,14 @@
+package io.mountblue.blogapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignupController {
+
+    @GetMapping("/signup")
+    public String signupHandler() {
+
+        return "Signup/Signup";
+    }
+}
